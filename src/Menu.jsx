662e3f6a320dfe9130dd51a542105bc4bc43e07b
Jesus Perez <jesusaperezs.05@gmail.comgit config --global user.email jesusaperezs.05@gmail.com>
2026-06-18 18:31:53 -0600

    feat: agregar botones imagenes y botones

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,13 +1,26 @@
 import Asteroides from "./Asteroides"
 import { Link } from "react-router-dom"
+import fondo from "./assets/fondo_4.jpg"
 
 function Menu() {
     return(
-        <div>
-            <h1 className="text-4xl font-bold text-center mb-2">
-        Bienvenido a la aventura espacial 🚀
-        </h1>
-        <Link to="/asteroides">Ver asteroides 🪨</Link>
+        <div
+        style={{backgroundImage: `url(${fondo})` }}
+        className="min-h-screen bg-cover bg-center flex flex-col items-start justify-start gap-4 p-12 text-white"
+        >
+            <h1 className="text-4xl font-bold text-center mb-2"> Bienvenido a la aventura espacial 🚀</h1>
+            <div className="flex flex-row gap-4">
+                <Link to="/asteroides" className="border border-white px-6 py-3 rounded hover:bg-white hover:text-black transition">
+                Lista Asteroides 🪨
+                </Link>
+                <Link to="/metricas" className="border border-white px-6 py-3 rounded hover:bg-white hover:text-black transition">
+                Métricas 📊
+                </Link>
+                <Link to="/ubicacion" className="border border-white px-6 py-3 rounded hover:bg-white hover:text-black transition">
+                Ubicación 📍
+                </Link>
+            </div>
+            
         </div>
     )
 }
