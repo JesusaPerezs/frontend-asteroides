64aa83c99437e38d2f72f77b42dbd2cdf597a3c3
Jesus Perez <jesusaperezs.05@gmail.comgit config --global user.email jesusaperezs.05@gmail.com>
2026-06-24 20:41:14 -0600

    mejorar visualizacion de botones

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 function Menu() {
     return(
         <div
-        className="min-h-screen bg-black bg-cover bg-center flex flex-col items-start padding-top pt-20 gap-.5 p-7 text-white"
+        className="min-h-screen bg-black bg-cover bg-center flex flex-col items-start pt-20 gap-.5 p-7 text-white"
         >
             <h1
             style={{fontFamily: "'Exo 2', sans-serif"}}
@@ -13,13 +13,13 @@ function Menu() {
                 SISTEMA DE <br /> DETECCIÓN DE <br /> ASTEROIDES
             </h1>
             <div className="flex flex-col gap-7 mt-[50px] ml-[20px]">
-                <Link to="/asteroides" className="border border-white/50 px-6 py-3 rounded hover:bg-white/20 hover:text-white transition">
+                <Link to="/asteroides" className="border border-cyan-400 shadow-lg shadow-cyan-500/30 px-5 py-4 rounded hover:bg-cyan-500/20 hover:shadow-cyan-200/10 tracking-widest uppercase text-sm hover:text-white transition">
                 Lista Asteroides
                 </Link>
-                <Link to="/metricas" className="border border-white/50 px-6 py-3 rounded hover:bg-white/20 hover:text-white transition">
+                <Link to="/metricas" className="border border-cyan-400 shadow-lg shadow-cyan-500/30 px-5 py-4 rounded hover:bg-cyan-500/20 hover:shadow-cyan-200/10 tracking-widest uppercase text-sm hover:text-white transition">
                 Métricas
                 </Link>
-                <Link to="/foto_dia" className="border border-white/50 px-6 py-3 rounded hover:bg-white/20 hover:text-white transition">
+                <Link to="/foto_dia" className="border border-cyan-400 shadow-lg shadow-cyan-500/30 px-5 py-4 rounded hover:bg-cyan-500/20 hover:shadow-cyan-200/10 tracking-widest uppercase text-sm hover:text-white transition">
                 Foto del día
                 </Link>
             </div>
